094658ef9a89e107e0e131cefddf16aa14ed7d7f
Metheal <metheal@outlook.com>
2021-02-03 23:47:43 +0300

    03.02.2021 Ders Sonu

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Product
+    public class Product : IEntity
     {
         public int ProductID { get; set; }
         public int CategoryID { get; set; }
